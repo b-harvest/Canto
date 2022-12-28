530b5ccf94bf44db1db15ef2e2fe5cac5b95de8e
xedonman <xedonman@gmail.com>
2022-12-28 17:35:51 +0900

    params go

--- x/liquidstaking/types/params.go
+++ x/liquidstaking/types/params.go
@@ -1,25 +1,100 @@
 package types
 
 import (
+	"fmt"
+	"strings"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 )
 
-func NewParams() Params {
-	return Params{}
+var (
+	ParamStoreKeyLiquidBondDenom        = []byte("ParamStoreKeyLiquidBondDenom")
+	ParamStoreKeyMinInsurancePercentage = []byte("ParamStoreKeyMinInsurancePercentage")
+	ParamStoreKeyChunkSize              = []byte("ParamStoreKeyChunkSize")
+	ParamStoreKeyMaxAliveChunk          = []byte("ParamStoreKeyMaxAliveChunk")
+
+	DefaultLiquidBondDenom        = "lsstake"
+	DefaultMinInsurancePercentage = sdk.NewDec(10)
+	DefaultChunkSize              = sdk.NewInt(500)
+	DefaultMaxAliveChunk          = sdk.NewInt(10)
+)
+
+func NewParams(liquidBondDenom string, minInsurancePercentage sdk.Dec, chunkSize, maxAliveChunk sdk.Int) Params {
+	return Params{
+		LiquidBondDenom:        liquidBondDenom,
+		MinInsurancePercentage: minInsurancePercentage,
+		ChunkSize:              chunkSize,
+		MaxAliveChunk:          maxAliveChunk,
+	}
 }
 
 func DefaultParams() Params {
-	return NewParams()
+	return NewParams(DefaultLiquidBondDenom, DefaultMinInsurancePercentage, DefaultChunkSize, DefaultMaxAliveChunk)
 }
 
-func (params *Params) Validate() error {
+func validateLiquidBondDenom(i interface{}) error {
+	v, ok := i.(string)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if strings.TrimSpace(v) == "" {
+		return fmt.Errorf("liquid bond denom cannot be blank")
+	}
+
+	if err := sdk.ValidateDenom(v); err != nil {
+		return err
+	}
 	return nil
 }
 
-// func (params Params) String() string {
-// 	out, _ := yaml.Marshal(params)
-// 	return string(out)
-// }
+func validateMinInsurancePercentage(i interface{}) error {
+	v, ok := i.(sdk.Dec)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+	maximumSlashablePercentage := sdk.ZeroDec()
+	if v.LT(maximumSlashablePercentage) {
+		return fmt.Errorf("minInsurancePercentage should be larger than maximum slashable percentage within an epoch")
+	}
+	return nil
+}
+
+func validateChunkSize(i interface{}) error {
+	v, ok := i.(sdk.Int)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+	if !v.IsPositive() {
+		return fmt.Errorf("chunk size should be larger than 0")
+	}
+	return nil
+}
+
+func validateMaxAliveChunk(i interface{}) error {
+	v, ok := i.(sdk.Int)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+	if !v.IsPositive() {
+		return fmt.Errorf("maxAliveChunk should be larger than 0")
+	}
+	return nil
+}
+
+func (params *Params) Validate() error {
+	if err := validateMinInsurancePercentage(params.MinInsurancePercentage); err != nil {
+		return err
+	}
+	if err := validateChunkSize(params.ChunkSize); err != nil {
+		return err
+	}
+	if err := validateMaxAliveChunk(params.MaxAliveChunk); err != nil {
+		return err
+	}
+	return nil
+}
 
 var _ paramtypes.ParamSet = (*Params)(nil)
 
@@ -28,5 +103,10 @@ func ParamKeyTable() paramtypes.KeyTable {
 }
 
 func (params *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{}
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(ParamStoreKeyLiquidBondDenom, &params.LiquidBondDenom, validateLiquidBondDenom),
+		paramtypes.NewParamSetPair(ParamStoreKeyMinInsurancePercentage, &params.MinInsurancePercentage, validateMinInsurancePercentage),
+		paramtypes.NewParamSetPair(ParamStoreKeyChunkSize, &params.ChunkSize, validateChunkSize),
+		paramtypes.NewParamSetPair(ParamStoreKeyMaxAliveChunk, &params.MaxAliveChunk, validateMaxAliveChunk),
+	}
 }
