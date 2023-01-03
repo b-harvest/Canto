2e37b2e703fde427f4250fb8b9bcb01406c4c142
xedonman <xedonman@gmail.com>
2023-01-03 14:20:04 +0900

    empty test

---
+++ x/liquidstaking/keeper/msg_server_test.go
@@ -0,0 +1,17 @@
+package keeper_test
+
+func (suite *KeeperTestSuite) TestLiquidStake() {
+	suite.Require().NoError(nil)
+}
+
+func (suite *KeeperTestSuite) TestLiquidUnstake() {
+	suite.Require().NoError(nil)
+}
+
+func (suite *KeeperTestSuite) TestRegisterInsurance() {
+	suite.Require().NoError(nil)
+}
+
+func (suite *KeeperTestSuite) TestUnregisterInsurance() {
+	suite.Require().NoError(nil)
+}
