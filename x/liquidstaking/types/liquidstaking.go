04ed396f634d4718beb7a765b9bc52c394cee889
xedonman <xedonman@gmail.com>
2023-01-06 14:23:03 +0900

    types liquidstaking

---
+++ x/liquidstaking/types/liquidstaking.go
@@ -0,0 +1,15 @@
+package types
+
+type LiquidValidators []LiquidValidator
+type LiquidValidatorStates []LiquidValidatorState
+
+type Chunks []Chunk
+type ChunkStates []ChunkState
+type ChunkId = uint64
+
+type Insurances []Insurance
+type InsuranceStates []InsuranceState
+type InsuranceId = uint64
+
+type Pairs []Pair
+type PairId = uint64
