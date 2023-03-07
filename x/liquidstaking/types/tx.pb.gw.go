b4c0cd703e993187307816ee029a9ede94ca1c5c
zsystm <actor93kor@gmail.com>
2023-03-07 11:41:38 +0900

    Add NetAmountState and do some chores

--- x/liquidstaking/types/tx.pb.gw.go
+++ x/liquidstaking/types/tx.pb.gw.go
@@ -250,37 +250,37 @@ func local_request_Msg_WithdrawInsurance_0(ctx context.Context, marshaler runtim
 }
 
 var (
-	filter_Msg_WithdrawInsuranceFee_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+	filter_Msg_WithdrawInsuranceCommission_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
 
-func request_Msg_WithdrawInsuranceFee_0(ctx context.Context, marshaler runtime.Marshaler, client MsgClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq MsgWithdrawInsuranceFee
+func request_Msg_WithdrawInsuranceCommission_0(ctx context.Context, marshaler runtime.Marshaler, client MsgClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq MsgWithdrawInsuranceCommission
 	var metadata runtime.ServerMetadata
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Msg_WithdrawInsuranceFee_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Msg_WithdrawInsuranceCommission_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := client.WithdrawInsuranceFee(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.WithdrawInsuranceCommission(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_Msg_WithdrawInsuranceFee_0(ctx context.Context, marshaler runtime.Marshaler, server MsgServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq MsgWithdrawInsuranceFee
+func local_request_Msg_WithdrawInsuranceCommission_0(ctx context.Context, marshaler runtime.Marshaler, server MsgServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq MsgWithdrawInsuranceCommission
 	var metadata runtime.ServerMetadata
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Msg_WithdrawInsuranceFee_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Msg_WithdrawInsuranceCommission_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := server.WithdrawInsuranceFee(ctx, &protoReq)
+	msg, err := server.WithdrawInsuranceCommission(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -429,7 +429,7 @@ func RegisterMsgHandlerServer(ctx context.Context, mux *runtime.ServeMux, server
 
 	})
 
-	mux.Handle("POST", pattern_Msg_WithdrawInsuranceFee_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_Msg_WithdrawInsuranceCommission_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -440,7 +440,7 @@ func RegisterMsgHandlerServer(ctx context.Context, mux *runtime.ServeMux, server
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_Msg_WithdrawInsuranceFee_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_Msg_WithdrawInsuranceCommission_0(rctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -448,7 +448,7 @@ func RegisterMsgHandlerServer(ctx context.Context, mux *runtime.ServeMux, server
 			return
 		}
 
-		forward_Msg_WithdrawInsuranceFee_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Msg_WithdrawInsuranceCommission_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -613,7 +613,7 @@ func RegisterMsgHandlerClient(ctx context.Context, mux *runtime.ServeMux, client
 
 	})
 
-	mux.Handle("POST", pattern_Msg_WithdrawInsuranceFee_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_Msg_WithdrawInsuranceCommission_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
@@ -622,14 +622,14 @@ func RegisterMsgHandlerClient(ctx context.Context, mux *runtime.ServeMux, client
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_Msg_WithdrawInsuranceFee_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_Msg_WithdrawInsuranceCommission_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_Msg_WithdrawInsuranceFee_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Msg_WithdrawInsuranceCommission_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -649,7 +649,7 @@ var (
 
 	pattern_Msg_WithdrawInsurance_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"canto", "liquidstaking", "v1", "tx", "withdraw_insurance"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Msg_WithdrawInsuranceFee_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"canto", "liquidstaking", "v1", "tx", "withdraw_insurance_fee"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Msg_WithdrawInsuranceCommission_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"canto", "liquidstaking", "v1", "tx", "withdraw_insurance_commission"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -665,5 +665,5 @@ var (
 
 	forward_Msg_WithdrawInsurance_0 = runtime.ForwardResponseMessage
 
-	forward_Msg_WithdrawInsuranceFee_0 = runtime.ForwardResponseMessage
+	forward_Msg_WithdrawInsuranceCommission_0 = runtime.ForwardResponseMessage
 )
