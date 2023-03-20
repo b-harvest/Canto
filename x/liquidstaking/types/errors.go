67787da2fc4abce66ee675c2cd72a3aa8cf147f5
zsystm <actor93kor@gmail.com>
2023-03-20 10:41:13 +0900

    Add missing error file

--- x/liquidstaking/types/errors.go
+++ x/liquidstaking/types/errors.go
@@ -3,5 +3,8 @@ package types
 import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 var (
-	ErrInsufficientBalance = sdkerrors.Register(ModuleName, 30000, "insufficient balance error")
+	ErrInsufficientBalance        = sdkerrors.Register(ModuleName, 30000, "insufficient balance error")
+	ErrMaxPairedChunkSizeExceeded = sdkerrors.Register(ModuleName, 30001, "reached maximum limit of paired chunk so cannot accept any more chunks.")
+	ErrNoPairingInsurance         = sdkerrors.Register(ModuleName, 30002, "pairing insurance must exist to accept liquid stake request.")
+	ErrInvalidAmount              = sdkerrors.Register(ModuleName, 30003, "amount of coin must be greater than or equal to 5M acanto.")
 )
