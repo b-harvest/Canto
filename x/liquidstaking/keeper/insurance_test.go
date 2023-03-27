dee3709944e1ad8e4de9ea2ccebb5da8cdfd570f
zsystm <actor93kor@gmail.com>
2023-03-27 17:24:18 +0900

    Add tc for sorting insurances and update tc helper

--- x/liquidstaking/keeper/insurance_test.go
+++ x/liquidstaking/keeper/insurance_test.go
@@ -3,8 +3,10 @@ package keeper_test
 import (
 	"github.com/Canto-Network/Canto/v6/x/liquidstaking/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/simulation"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/evmos/ethermint/tests"
+	"math/rand"
 )
 
 // Sets a bunch of insurances in the store and then get and ensure that each of them
@@ -70,6 +72,9 @@ func (suite *KeeperTestSuite) TestLastInsuranceIdSetGet() {
 
 // Creates a bunch of insurances
 func GenerateInsurances(number int, sameAddress bool) []types.Insurance {
+	s := rand.NewSource(0)
+	r := rand.New(s)
+
 	insurances := make([]types.Insurance, number)
 	for i := 0; i < number; i++ {
 		var addr string
@@ -79,7 +84,8 @@ func GenerateInsurances(number int, sameAddress bool) []types.Insurance {
 			addr = sdk.AccAddress(tests.GenerateAddress().Bytes()).String()
 		}
 
-		insurances[i] = types.NewInsurance(uint64(i), addr, "", sdk.NewDec(0))
+		randomFee := sdk.NewDecWithPrec(int64(simulation.RandIntBetween(r, 1, 99)), 2)
+		insurances[i] = types.NewInsurance(uint64(i), addr, "", randomFee)
 		insurances[i].ProviderAddress = addr
 	}
 	return insurances
