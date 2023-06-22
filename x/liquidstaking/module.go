70f00d57585b3c55774c387f269be696d83c53d5
zsystm <actor93kor@gmail.com>
2023-06-22 11:50:12 +0900

    apply endblocker

--- x/liquidstaking/module.go
+++ x/liquidstaking/module.go
@@ -155,8 +155,8 @@ func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {
 
 // EndBlock executes all ABCI EndBlock logic respective to the liquidstaking module. It
 // returns no validator updates.
-func (am AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
-	return []abci.ValidatorUpdate{}
+func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) {
+	EndBlocker(ctx, am.keeper)
 }
 
 // InitGenesis performs the liquidstaking module's genesis initialization. It returns
