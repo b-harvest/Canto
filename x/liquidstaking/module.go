6d9e6a0cdd94c8e46fa1236d224902c322d4ca7e
zsystm <actor93kor@gmail.com>
2023-07-24 20:50:57 +0900

    wip: refactor simulation codes
    todo: need to run it in local

--- x/liquidstaking/module.go
+++ x/liquidstaking/module.go
@@ -4,6 +4,18 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	inflationkeeper "github.com/Canto-Network/Canto/v6/x/inflation/keeper"
+	inflationtypes "github.com/Canto-Network/Canto/v6/x/inflation/types"
+	"github.com/armon/go-metrics"
+	"github.com/cosmos/cosmos-sdk/telemetry"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	distrkeeper "github.com/cosmos/cosmos-sdk/x/distribution/keeper"
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	ethermint "github.com/evmos/ethermint/types"
 	"math/rand"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -98,6 +110,10 @@ type AppModule struct {
 	AppModuleBasic
 	keeper keeper.Keeper
 	ak     authkeeper.AccountKeeper
+	bk     bankkeeper.BaseKeeper
+	sk     stakingkeeper.Keeper
+	dk     distrkeeper.Keeper
+	ik     inflationkeeper.Keeper
 }
 
 // NewAppModule creates a new AppModule Object
@@ -202,4 +218,115 @@ func (am AppModule) RegisterStoreDecoder(decoderRegistry sdk.StoreDecoderRegistr
 // WeightedOperations returns liquidstaking module weighted operations
 func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
 	return []simtypes.WeightedOperation{}
+}
+
+func (am AppModule) AdvanceEpochBeginBlock(ctx sdk.Context) {
+	bondDenom := am.sk.BondDenom(ctx)
+	lsmEpoch := am.keeper.GetEpoch(ctx)
+	ctx = ctx.WithBlockTime(lsmEpoch.StartTime.Add(lsmEpoch.Duration))
+	staking.BeginBlocker(ctx, am.sk)
+
+	// mimic the begin block logic of epoch module
+	// currently epoch module use hooks when begin block and inflation module
+	// implemented that hook, so actual logic is in inflation module.
+	{
+		epochMintProvision, found := am.ik.GetEpochMintProvision(ctx)
+		if !found {
+			panic("epoch mint provision not found")
+		}
+		inflationParams := am.ik.GetParams(ctx)
+		// mintedCoin := sdk.NewCoin(inflationParams.MintDenom, epochMintProvision.TruncateInt())
+		mintedCoin := sdk.NewCoin(inflationParams.MintDenom, sdk.TokensFromConsensusPower(100, ethermint.PowerReduction))
+		staking, communityPool, err := am.ik.MintAndAllocateInflation(ctx, mintedCoin)
+		if err != nil {
+			panic(err)
+		}
+		defer func() {
+			if mintedCoin.Amount.IsInt64() {
+				telemetry.IncrCounterWithLabels(
+					[]string{"inflation", "allocate", "total"},
+					float32(mintedCoin.Amount.Int64()),
+					[]metrics.Label{telemetry.NewLabel("denom", mintedCoin.Denom)},
+				)
+			}
+			if staking.AmountOf(mintedCoin.Denom).IsInt64() {
+				telemetry.IncrCounterWithLabels(
+					[]string{"inflation", "allocate", "staking", "total"},
+					float32(staking.AmountOf(mintedCoin.Denom).Int64()),
+					[]metrics.Label{telemetry.NewLabel("denom", mintedCoin.Denom)},
+				)
+			}
+			if communityPool.AmountOf(mintedCoin.Denom).IsInt64() {
+				telemetry.IncrCounterWithLabels(
+					[]string{"inflation", "allocate", "community_pool", "total"},
+					float32(communityPool.AmountOf(mintedCoin.Denom).Int64()),
+					[]metrics.Label{telemetry.NewLabel("denom", mintedCoin.Denom)},
+				)
+			}
+		}()
+
+		ctx.EventManager().EmitEvent(
+			sdk.NewEvent(
+				inflationtypes.EventTypeMint,
+				sdk.NewAttribute(inflationtypes.AttributeEpochNumber, fmt.Sprintf("%d", -1)),
+				sdk.NewAttribute(inflationtypes.AttributeKeyEpochProvisions, epochMintProvision.String()),
+				sdk.NewAttribute(sdk.AttributeKeyAmount, mintedCoin.Amount.String()),
+			),
+		)
+	}
+
+	feeCollector := am.ak.GetModuleAccount(ctx, authtypes.FeeCollectorName)
+	// mimic the begin block logic of distribution module
+	{
+		feeCollectorBalance := am.bk.SpendableCoins(ctx, feeCollector.GetAddress())
+		rewardsToBeDistributed := feeCollectorBalance.AmountOf(bondDenom)
+
+		// mimic distribution.BeginBlock (AllocateTokens, get rewards from feeCollector, AllocateTokensToValidator, add remaining to feePool)
+		err := am.bk.SendCoinsFromModuleToModule(ctx, authtypes.FeeCollectorName, distrtypes.ModuleName, feeCollectorBalance)
+		if err != nil {
+			panic(err)
+		}
+		totalRewards := sdk.ZeroDec()
+		totalPower := int64(0)
+		am.sk.IterateBondedValidatorsByPower(ctx, func(index int64, validator stakingtypes.ValidatorI) (stop bool) {
+			consPower := validator.GetConsensusPower(am.sk.PowerReduction(ctx))
+			totalPower = totalPower + consPower
+			return false
+		})
+		if totalPower != 0 {
+			am.sk.IterateBondedValidatorsByPower(ctx, func(index int64, validator stakingtypes.ValidatorI) (stop bool) {
+				consPower := validator.GetConsensusPower(am.sk.PowerReduction(ctx))
+				powerFraction := sdk.NewDec(consPower).QuoTruncate(sdk.NewDec(totalPower))
+				reward := rewardsToBeDistributed.ToDec().MulTruncate(powerFraction)
+				am.dk.AllocateTokensToValidator(ctx, validator, sdk.DecCoins{{Denom: bondDenom, Amount: reward}})
+				totalRewards = totalRewards.Add(reward)
+				return false
+			})
+		}
+		remaining := rewardsToBeDistributed.ToDec().Sub(totalRewards)
+		feePool := am.dk.GetFeePool(ctx)
+		feePool.CommunityPool = feePool.CommunityPool.Add(sdk.DecCoins{
+			{Denom: bondDenom, Amount: remaining}}...)
+		am.dk.SetFeePool(ctx, feePool)
+	}
+	am.keeper.CoverRedelegationPenalty(ctx)
+}
+
+func (am AppModule) AdvanceEpochEndBlock(ctx sdk.Context) {
+	lsmEpoch := am.keeper.GetEpoch(ctx)
+	ctx = ctx.WithBlockTime(lsmEpoch.StartTime.Add(lsmEpoch.Duration))
+
+	staking.EndBlocker(ctx, am.sk)
+	// mimic liquidstaking endblocker except increasing epoch
+	{
+		am.keeper.DistributeReward(ctx)
+		am.keeper.CoverSlashingAndHandleMatureUnbondings(ctx)
+		am.keeper.RemoveDeletableRedelegationInfos(ctx)
+		am.keeper.HandleQueuedLiquidUnstakes(ctx)
+		am.keeper.HandleUnprocessedQueuedLiquidUnstakes(ctx)
+		am.keeper.HandleQueuedWithdrawInsuranceRequests(ctx)
+		newlyRankedInInsurances, rankOutInsurances := am.keeper.RankInsurances(ctx)
+		am.keeper.RePairRankedInsurances(ctx, newlyRankedInInsurances, rankOutInsurances)
+		am.keeper.IncrementEpoch(ctx)
+	}
 }
