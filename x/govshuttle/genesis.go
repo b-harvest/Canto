052b76ac5355c716b526b6f8bede6bb7ea1f5524
poorphd <bc.park@bharvest.io>
2023-08-24 17:09:00 +0900

    fix: genesis import/export for govshuttle module

--- x/govshuttle/genesis.go
+++ x/govshuttle/genesis.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Canto-Network/Canto/v7/x/govshuttle/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 // InitGenesis initializes the capability module's state from a provided genesis
@@ -12,16 +13,17 @@ import (
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, accountKeeper authkeeper.AccountKeeper, genState types.GenesisState) {
 	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
-
+	k.SetPort(ctx, common.HexToAddress(genState.PortAddress))
 	if acc := accountKeeper.GetModuleAccount(ctx, types.ModuleName); acc == nil {
 		panic("the govshuttle module account has not been set")
 	}
+
 }
 
 // ExportGenesis returns the capability module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
-	genesis := types.DefaultGenesis()
-	genesis.Params = k.GetParams(ctx)
+	portAddress, _ := k.GetPort(ctx)
+	genesis := types.NewGenesisState(k.GetParams(ctx), portAddress)
 
 	// this line is used by starport scaffolding # genesis/module/export
 
--- x/govshuttle/types/genesis.go
+++ x/govshuttle/types/genesis.go
@@ -1,17 +1,20 @@
 package types
 
-import (
-// this line is used by starport scaffolding # genesis/types/import
-)
+import "github.com/ethereum/go-ethereum/common"
 
 // DefaultIndex is the default capability global index
 const DefaultIndex uint64 = 1
 
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
+	return NewGenesisState(DefaultParams(), common.Address{})
+}
+
+func NewGenesisState(params Params, portAddress common.Address) *GenesisState {
 	return &GenesisState{
-		// this line is used by starport scaffolding # genesis/types/default
-		Params: DefaultParams(),
+		Params:      params,
+		PortAddress: portAddress.String(),
+		// this line is used by starport scaffolding # genesis/types/init
 	}
 }
 
